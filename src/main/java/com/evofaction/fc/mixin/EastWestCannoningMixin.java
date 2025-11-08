cab21045d3279c016430068008d62f50a2744d79
UltimateGG <clubblock1@gmail.com>
2025-11-08 17:29:56 -0600

    Add lithium, make compatible

--- src/main/java/com/evofaction/fc/mixin/EastWestCannoningMixin.java
+++ src/main/java/com/evofaction/fc/mixin/EastWestCannoningMixin.java
@@ -1,52 +1,112 @@
 package com.evofaction.fc.mixin;
 
 import com.evofaction.fc.Config;
+import me.jellysquid.mods.lithium.common.entity.LithiumEntityCollisions;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.util.List;
 
 
+// Overwrites lithium's entity.collisions.movement. See fabric.mod.json
 @Mixin(Entity.class)
 public class EastWestCannoningMixin {
+    @Redirect(
+        method = "adjustMovementForCollisions(Lnet/minecraft/util/math/Vec3d;)Lnet/minecraft/util/math/Vec3d;",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/entity/Entity;adjustMovementForCollisions(Lnet/minecraft/entity/Entity;Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/util/math/Box;Lnet/minecraft/world/World;Ljava/util/List;)Lnet/minecraft/util/math/Vec3d;"
+        ),
+        require = 5
+    )
+    private Vec3d adjustMovementForCollisionsGetEntitiesLater(@Nullable Entity entity, Vec3d movement, Box entityBoundingBox, World world, List<VoxelShape> collisions) {
+        return lithiumCollideMultiAxisMovement(entity, movement, entityBoundingBox, world, true, collisions);
+    }
+
     /**
-     * @author UltimateGamer079
-     * @reason Mojang actually implemented an "e/w patch", in this method.
-     *         But it is opposite of what we want in that it does the <b>greater</b> velocity <b>first</b>.
-     *         <br/><br/>
-     *         The idea of the e/w patch is that it infers the direction you intend to shoot in based
-     *         on which axis has a greater velocity, since usually you give more exposure to that direction.
-     *         <br/><br/>
-     *         For example, if there is more X velocity, you are (probably!) shooting towards X.
-     *         So to ensure the "floating" of a cannon works, we calculate the opposite axis you are
-     *         shooting in as the first (after Y) leg of the triangle, Z in this case.
-     *         This is so that it hits the left/right guider first, instead of going towards X first
-     *         and sliding across Z at the wall.
-     *         <br/><br/>
-     *         "probably" is a key word there. This is not foolproof, but it's how most servers did it.
-     *         It WILL break some cannons (e.g. diags) which is why I prefer it off.
+     * @author 2No2Name
+     * @reason Replace with optimized implementation
      */
     @Overwrite
-    private static Vec3d adjustMovementForCollisions(Vec3d movement, Box entityBoundingBox, List<VoxelShape> collisions) {
-        if (collisions.isEmpty()) return movement;
+    public static Vec3d adjustMovementForCollisions(@Nullable Entity entity, Vec3d movement, Box entityBoundingBox, World world, List<VoxelShape> collisions) {
+        return lithiumCollideMultiAxisMovement(entity, movement, entityBoundingBox, world, false, collisions);
+    }
 
-        double x = movement.x;
-        double y = movement.y;
-        double z = movement.z;
-
-        if (y != 0.0) {
-            y = VoxelShapes.calculateMaxOffset(Direction.Axis.Y, entityBoundingBox, collisions, y);
-            if (y != 0.0) {
-                entityBoundingBox = entityBoundingBox.offset(0.0, y, 0.0);
+    /**
+     * @author 2No2Name, UltimateGamer079
+     * @implNote Mojang actually implemented an "e/w patch", in this method.
+     *  But it is opposite of what we want in that it does the <b>greater</b> velocity <b>first</b>.
+     *  <br/><br/>
+     *  The idea of the e/w patch is that it infers the direction you intend to shoot in based
+     *  on which axis has a greater velocity, since usually you give more exposure to that direction.
+     *  <br/><br/>
+     *  For example, if there is more X velocity, you are (probably!) shooting towards X.
+     *  So to ensure the "floating" of a cannon works, we calculate the opposite axis you are
+     *  shooting in as the first (after Y) leg of the triangle, Z in this case.
+     *  This is so that it hits the left/right guider first, instead of going towards X first
+     *  and sliding across Z at the wall.
+     *  <br/><br/>
+     *  "probably" is a key word there. This is not foolproof, but it's how most servers did it.
+     *  It WILL break some cannons (e.g. diags) which is why I prefer it off.
+     */
+    // https://github.com/CaffeineMC/lithium/blob/1.20.1/src/main/java/me/jellysquid/mods/lithium/mixin/entity/collisions/movement/EntityMixin.java
+    @Unique
+    private static Vec3d lithiumCollideMultiAxisMovement(@Nullable Entity entity, Vec3d movement, Box entityBoundingBox, World world, boolean getEntityCollisions, List<VoxelShape> otherCollisions) {
+        //vanilla order: entities, worldborder, blocks. It is unknown whether changing this order changes the result regarding the confusing 1e-7 VoxelShape margin behavior. Not yet investigated
+        double velX = movement.x;
+        double velY = movement.y;
+        double velZ = movement.z;
+        boolean isVerticalOnly = velX == 0 && velZ == 0;
+        Box movementSpace;
+        if (isVerticalOnly) {
+            if (velY < 0) {
+                //Check block directly below center of entity first
+                VoxelShape voxelShape = LithiumEntityCollisions.getCollisionShapeBelowEntity(world, entity, entityBoundingBox);
+                if (voxelShape != null) {
+                    double v = voxelShape.calculateMaxDistance(Direction.Axis.Y, entityBoundingBox, velY);
+                    if (v == 0) {
+                        return Vec3d.ZERO;
+                    }
+                }
+                //Reduced collision volume optimization for entities that are just standing around
+                movementSpace = new Box(entityBoundingBox.minX, entityBoundingBox.minY + velY, entityBoundingBox.minZ, entityBoundingBox.maxX, entityBoundingBox.minY, entityBoundingBox.maxZ);
+            } else {
+                movementSpace = new Box(entityBoundingBox.minX, entityBoundingBox.maxY, entityBoundingBox.minZ, entityBoundingBox.maxX, entityBoundingBox.maxY + velY, entityBoundingBox.maxZ);
             }
+        } else {
+            movementSpace = entityBoundingBox.stretch(movement);
         }
 
+        List<VoxelShape> blockCollisions = LithiumEntityCollisions.getBlockCollisions(world, entity, movementSpace);
+        List<VoxelShape> entityWorldBorderCollisions = null;
+
+        if (velY != 0.0) {
+            velY = VoxelShapes.calculateMaxOffset(Direction.Axis.Y, entityBoundingBox, blockCollisions, velY);
+            if (velY != 0.0) {
+                if (!otherCollisions.isEmpty()) {
+                    velY = VoxelShapes.calculateMaxOffset(Direction.Axis.Y, entityBoundingBox, otherCollisions, velY);
+                }
+                if (velY != 0.0 && getEntityCollisions) {
+                    entityWorldBorderCollisions = LithiumEntityCollisions.getEntityWorldBorderCollisions(world, entity, movementSpace, entity != null);
+                    velY = VoxelShapes.calculateMaxOffset(Direction.Axis.Y, entityBoundingBox, entityWorldBorderCollisions, velY);
+                }
+                if (velY != 0.0) {
+                    entityBoundingBox = entityBoundingBox.offset(0.0, velY, 0.0);
+                }
+            }
+        }
+        // FabricCannoning Start
         // Note: OneshotMC classic ("legacy") jar would calculate Z first if
         // the velocities were equal. I want to default to vanilla if they are
         // equal though. If we ever want to change that make it >= here.
@@ -54,27 +114,60 @@ public class EastWestCannoningMixin {
 
         // noinspection SimplifiableConditionalExpression
         boolean calculateZFirst = Config.EAST_WEST_CANNONING_FIX
-            ? Math.abs(x) > Math.abs(z) // Flipped condition (relative to Mojangs) so the == edge case lines up (If equal, use YXZ)
+            ? Math.abs(velX) > Math.abs(velZ) // Flipped condition (relative to Mojangs) so the == edge case lines up (If equal, use YXZ)
             : false; // 1.8 behavior (never change YXZ order)
+        // FabricCannoning End
+        if (calculateZFirst) {
+            velZ = VoxelShapes.calculateMaxOffset(Direction.Axis.Z, entityBoundingBox, blockCollisions, velZ);
+            if (velZ != 0.0) {
+                if (!otherCollisions.isEmpty()) {
+                    velZ = VoxelShapes.calculateMaxOffset(Direction.Axis.Z, entityBoundingBox, otherCollisions, velZ);
+                }
+                if (velZ != 0.0 && getEntityCollisions) {
+                    if (entityWorldBorderCollisions == null) {
+                        entityWorldBorderCollisions = LithiumEntityCollisions.getEntityWorldBorderCollisions(world, entity, movementSpace, entity != null);
+                    }
 
-        if (calculateZFirst && z != 0.0) {
-            z = VoxelShapes.calculateMaxOffset(Direction.Axis.Z, entityBoundingBox, collisions, z);
-            if (z != 0.0) {
-                entityBoundingBox = entityBoundingBox.offset(0.0, 0.0, z);
+                    velZ = VoxelShapes.calculateMaxOffset(Direction.Axis.Z, entityBoundingBox, entityWorldBorderCollisions, velZ);
+                }
+                if (velZ != 0.0) {
+                    entityBoundingBox = entityBoundingBox.offset(0.0, 0.0, velZ);
+                }
             }
         }
+        if (velX != 0.0) {
+            velX = VoxelShapes.calculateMaxOffset(Direction.Axis.X, entityBoundingBox, blockCollisions, velX);
+            if (velX != 0.0) {
+                if (!otherCollisions.isEmpty()) {
+                    velX = VoxelShapes.calculateMaxOffset(Direction.Axis.X, entityBoundingBox, otherCollisions, velX);
+                }
+                if (velX != 0.0 && getEntityCollisions) {
+                    if (entityWorldBorderCollisions == null) {
+                        entityWorldBorderCollisions = LithiumEntityCollisions.getEntityWorldBorderCollisions(world, entity, movementSpace, entity != null);
+                    }
 
-        if (x != 0.0) {
-            x = VoxelShapes.calculateMaxOffset(Direction.Axis.X, entityBoundingBox, collisions, x);
-            if (!calculateZFirst && x != 0.0) {
-                entityBoundingBox = entityBoundingBox.offset(x, 0.0, 0.0);
+                    velX = VoxelShapes.calculateMaxOffset(Direction.Axis.X, entityBoundingBox, entityWorldBorderCollisions, velX);
+                }
+                if (velX != 0.0) {
+                    entityBoundingBox = entityBoundingBox.offset(velX, 0.0, 0.0);
+                }
             }
         }
+        if (!calculateZFirst && velZ != 0.0) {
+            velZ = VoxelShapes.calculateMaxOffset(Direction.Axis.Z, entityBoundingBox, blockCollisions, velZ);
+            if (velZ != 0.0) {
+                if (!otherCollisions.isEmpty()) {
+                    velZ = VoxelShapes.calculateMaxOffset(Direction.Axis.Z, entityBoundingBox, otherCollisions, velZ);
+                }
+                if (velZ != 0.0 && getEntityCollisions) {
+                    if (entityWorldBorderCollisions == null) {
+                        entityWorldBorderCollisions = LithiumEntityCollisions.getEntityWorldBorderCollisions(world, entity, movementSpace, entity != null);
+                    }
 
-        if (!calculateZFirst && z != 0.0) {
-            z = VoxelShapes.calculateMaxOffset(Direction.Axis.Z, entityBoundingBox, collisions, z);
+                    velZ = VoxelShapes.calculateMaxOffset(Direction.Axis.Z, entityBoundingBox, entityWorldBorderCollisions, velZ);
+                }
+            }
         }
-
-        return new Vec3d(x, y, z);
+        return new Vec3d(velX, velY, velZ);
     }
 }
--- src/main/java/com/evofaction/fc/mixin/ExplosionMixin.java
+++ src/main/java/com/evofaction/fc/mixin/ExplosionMixin.java
@@ -2,106 +2,37 @@ package com.evofaction.fc.mixin;
 
 import com.evofaction.fc.Config;
 import com.evofaction.fc.server.ExposureCache;
-import com.evofaction.fc.server.ProtectionBlock;
-import com.google.common.collect.Sets;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
-import net.minecraft.block.BlockState;
-import net.minecraft.enchantment.ProtectionEnchantment;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.fluid.FluidState;
-import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Box;
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
-import net.minecraft.world.RaycastContext;
-import net.minecraft.world.World;
-import net.minecraft.world.event.GameEvent;
 import net.minecraft.world.explosion.Explosion;
-import net.minecraft.world.explosion.ExplosionBehavior;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.*;
-
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 
 @Mixin(Explosion.class)
 public class ExplosionMixin {
-    @Final @Shadow
-    private World world;
-
-    @Final @Shadow
-    private double x;
-
-    @Final @Shadow
-    private double y;
-
-    @Final @Shadow
-    private double z;
-
-    @Final @Shadow @Nullable
-    private Entity entity;
-
-    @Final @Shadow
-    private float power;
-
-    @Final @Shadow
-    private ExplosionBehavior behavior;
-
-    @Final @Shadow
+    @Shadow @Final
     private ObjectArrayList<BlockPos> affectedBlocks;
 
-    @Shadow @Final
-    private Map<PlayerEntity, Vec3d> affectedPlayers;
-
-    /**
-     * @author UltimateGamer079
-     * @reason Testing exposure
-     */
-    @Overwrite
+    // Does not actually return 0, just here for compilation.
+    @Shadow
     public static float getExposure(Vec3d source, Entity entity) {
-        Box box = entity.getBoundingBox();
-        double d = 1.0 / ((box.maxX - box.minX) * 2.0 + 1.0);
-        double e = 1.0 / ((box.maxY - box.minY) * 2.0 + 1.0);
-        double f = 1.0 / ((box.maxZ - box.minZ) * 2.0 + 1.0);
-        double g = (1.0 - Math.floor(1.0 / d) * d) / 2.0;
-        double h = (1.0 - Math.floor(1.0 / f) * f) / 2.0;
-        if (!(d < 0.0) && !(e < 0.0) && !(f < 0.0)) {
-            int i = 0;
-            int j = 0;
-
-            for (double k = 0.0; k <= 1.0; k += d) {
-                for (double l = 0.0; l <= 1.0; l += e) {
-                    for (double m = 0.0; m <= 1.0; m += f) {
-                        double n = MathHelper.lerp(k, box.minX, box.maxX);
-                        double o = MathHelper.lerp(l, box.minY, box.maxY);
-                        double p = MathHelper.lerp(m, box.minZ, box.maxZ);
-                        Vec3d vec3d = new Vec3d(n + g, o, p + h);
-                        if (entity.getWorld().raycast(new RaycastContext(vec3d, source, RaycastContext.ShapeType.COLLIDER, RaycastContext.FluidHandling.NONE, entity)).getType()
-                            == HitResult.Type.MISS) {
-                            i++;
-                        }
-
-                        j++;
-                    }
-                }
-            }
-
-//            System.out.println("Exposure: " + i + " / " + j + " hits");
-            return (float)i / j;
-        } else {
-            return 0.0F;
-        }
+        return 0;
     }
 
-    @Unique
-    private static float cachedGetExposure(Vec3d source, Entity entity) {
+    @Redirect(
+        method = "collectBlocksAndDamageEntities",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/world/explosion/Explosion;getExposure(Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/entity/Entity;)F"
+        )
+    )
+    private float cachedGetExposure(Vec3d source, Entity entity) {
         if (!Config.CACHE_EXPLOSION_EXPOSURE) return getExposure(source, entity);
 
         ExposureCache.CacheKey key = new ExposureCache.CacheKey(entity.getWorld(), source, entity.getBoundingBox());
@@ -113,122 +44,46 @@ public class ExplosionMixin {
         return value;
     }
 
-    @Shadow @Final
-    private DamageSource damageSource;
-
     /**
-     * Old MC casted to a float and back to a double.
+     * Old MC MathHelper.sqrt casted to a float and back to a double.
      * This causes tiny decimal differences compared to 1.20.
      */
-    @Unique
-    private static double legacySqrt(double v) {
-        return (double) (float) Math.sqrt(v);
+    @Redirect(
+        method = "collectBlocksAndDamageEntities",
+        at = @At(
+            value = "INVOKE",
+            target = "Ljava/lang/Math;sqrt(D)D",
+            remap = false
+        )
+    )
+    private double legacySqrt(double dist) {
+        return (float) Math.sqrt(dist);
     }
 
-    /**
-     * @author UltimateGamer079
-     * @reason Lots to be done to this method. Future optimizations.
-     */
-    @Overwrite
-    public void collectBlocksAndDamageEntities() {
-        Explosion self = (Explosion) (Object) this;
-        this.world.emitGameEvent(this.entity, GameEvent.EXPLODE, new Vec3d(this.x, this.y, this.z));
-        Set<BlockPos> set = Sets.<BlockPos>newHashSet();
-        int i = 16;
+    // Effectivley removes mojang instanceof tnt entity check to align with 1.8
+    @Redirect(
+        method = "collectBlocksAndDamageEntities",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/entity/Entity;getY()D"
+        )
+    )
+    private double fixEyeHeight(Entity instance) {
+        return instance.getEyeY();
+    }
 
-        // Collecting blocks to affect for explosions, and potentially spawning fire
-        if (!Config.PROTECTION_BLOCK_ENABLED || !ProtectionBlock.isProtectedColumn(this.world, MathHelper.floor(this.x), MathHelper.floor(this.z))) {
-            for (int j = 0; j < 16; j++) {
-                for (int k = 0; k < 16; k++) {
-                    for (int l = 0; l < 16; l++) {
-                        if (j == 0 || j == 15 || k == 0 || k == 15 || l == 0 || l == 15) {
-                            double d = j / 15.0F * 2.0F - 1.0F;
-                            double e = k / 15.0F * 2.0F - 1.0F;
-                            double f = l / 15.0F * 2.0F - 1.0F;
-                            double g = Math.sqrt(d * d + e * e + f * f);
-                            d /= g;
-                            e /= g;
-                            f /= g;
-                            float h = this.power * (0.7F + this.world.random.nextFloat() * 0.6F);
-                            double m = this.x;
-                            double n = this.y;
-                            double o = this.z;
+    @Inject(
+        method = "collectBlocksAndDamageEntities",
+        at = @At(
+            value = "INVOKE",
+            target = "Lit/unimi/dsi/fastutil/objects/ObjectArrayList;addAll(Ljava/util/Collection;)Z",
+            shift = At.Shift.AFTER,
+            remap = false
+        )
+    )
+    private void cannonProtection(CallbackInfo ci) {
+        if (!Config.PROTECTION_BLOCK_ENABLED) return;
 
-                            for (float p = 0.3F; h > 0.0F; h -= 0.22500001F) {
-                                BlockPos blockPos = BlockPos.ofFloored(m, n, o);
-                                BlockState blockState = this.world.getBlockState(blockPos);
-                                FluidState fluidState = this.world.getFluidState(blockPos);
-                                if (!this.world.isInBuildLimit(blockPos)) {
-                                    break;
-                                }
-
-                                Optional<Float> optional = this.behavior.getBlastResistance(self, this.world, blockPos, blockState, fluidState);
-                                if (optional.isPresent()) {
-                                    h -= (optional.get() + 0.3F) * 0.3F;
-                                }
-
-                                if (h > 0.0F && this.behavior.canDestroyBlock(self, this.world, blockPos, blockState, h)) {
-                                    if (!Config.PROTECTION_BLOCK_ENABLED
-                                        || !ProtectionBlock.isProtectedColumn(this.world, blockPos.getX(), blockPos.getZ()))
-                                        set.add(blockPos);
-                                }
-
-                                m += d * 0.3F;
-                                n += e * 0.3F;
-                                o += f * 0.3F;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        this.affectedBlocks.addAll(set);
-        float q = this.power * 2.0F;
-        int k = MathHelper.floor(this.x - q - 1.0);
-        int lx = MathHelper.floor(this.x + q + 1.0);
-        int r = MathHelper.floor(this.y - q - 1.0);
-        int s = MathHelper.floor(this.y + q + 1.0);
-        int t = MathHelper.floor(this.z - q - 1.0);
-        int u = MathHelper.floor(this.z + q + 1.0);
-        List<Entity> list = this.world.getOtherEntities(this.entity, new Box(k, r, t, lx, s, u));
-        Vec3d vec3d = new Vec3d(this.x, this.y, this.z);
-
-        for (int v = 0; v < list.size(); v++) {
-            Entity entity = (Entity)list.get(v);
-            if (!entity.isImmuneToExplosion()) {
-                double w = legacySqrt(entity.squaredDistanceTo(vec3d)) / q;
-                if (w <= 1.0) {
-                    double x = entity.getX() - this.x;
-                    // Removed mojang instanceof tnt entity check to align with 1.8
-                    double y = entity.getEyeY() - this.y;
-                    double z = entity.getZ() - this.z;
-                    double aa = legacySqrt(x * x + y * y + z * z);
-                    if (aa != 0.0) {
-                        x /= aa;
-                        y /= aa;
-                        z /= aa;
-                        double ab = cachedGetExposure(vec3d, entity);
-                        double ac = (1.0 - w) * ab;
-                        entity.damage(this.damageSource, (int)((ac * ac + ac) / 2.0 * 7.0 * q + 1.0));
-                        double ad;
-                        if (entity instanceof LivingEntity livingEntity) {
-                            ad = ProtectionEnchantment.transformExplosionKnockback(livingEntity, ac);
-                        } else {
-                            ad = ac;
-                        }
-
-                        x *= ad;
-                        y *= ad;
-                        z *= ad;
-                        Vec3d vec3d2 = new Vec3d(x, y, z);
-                        entity.setVelocity(entity.getVelocity().add(vec3d2));
-                        if (entity instanceof PlayerEntity playerEntity && !playerEntity.isSpectator() && (!playerEntity.isCreative() || !playerEntity.getAbilities().flying)) {
-                            this.affectedPlayers.put(playerEntity, vec3d2);
-                        }
-                    }
-                }
-            }
-        }
+        // TODO
     }
 }
